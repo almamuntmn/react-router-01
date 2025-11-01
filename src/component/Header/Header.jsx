88401564942419c1f2c53e1043fe30d98f3b55cb
almamuntmn <almamuncool88@gmail.com>
2025-11-01 13:22:17 +0600

    navlink

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>This is Header</h1>
+            <nav>
+                <NavLink to='/'>Home</NavLink> 
+                <NavLink to='/Mobiles'>Mobiles</NavLink> 
+                <NavLink to='/Contact'>Contact</NavLink>
+                <NavLink to='/app'>App</NavLink>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,34 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from './component/Root/Root.jsx';
+import Home from './component/Home/Home.jsx';
+import Mobiles from './component/Mobiles/Mobiles.jsx';
+import Contact from './component/Contact/Contact.jsx';
 
 const router=createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>
+    element: <Root></Root>,
+    children:[
+      {index:true, element:<Home></Home>},
+      {path: 'mobiles', element:<Mobiles></Mobiles>},
+      {path: 'contact', element:<Contact></Contact>},
+      {path: 'app', element:<App></App>}
+    ]
   },
+  {
+    path: "/about",
+    element: <div>About</div>
+  },
+  {
+    path: "/contact",
+    element: <div>Contact</div>
+  },
+  {
+    path: "/app",
+    Component: App
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
